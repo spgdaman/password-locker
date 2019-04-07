16d9ebac9296e376d2ded0cfa40cb46f1d9b1693
spgdaman <gatheru.simon432@gmail.com>
2019-04-07 15:13:10 +0300

    define function for authentication, credentials creation and saving credentials

--- run_pass_lock.py
+++ run_pass_lock.py
@@ -12,3 +12,30 @@ def register_user(user):
 	'''
 	Users.create_user(user)
 	
+def user_check(first,pwd):
+	'''
+	Does an authorization check prior to creating any credentials
+	'''
+	return Credentials.user_check(first,pwd)
+
+def create_cred(name,username,platform,pwd):
+	'''
+	Creates credentials to be saved
+	'''
+	create_instance = Credentials(name,username,platform,pwd)
+	return create_instance
+	
+def save_cred(cred):
+	'''
+	Saves created credentials
+	'''
+	Credentials.save_cred(cred)
+	
+	
+	
+	
+	
+	
+	
+	
+	
