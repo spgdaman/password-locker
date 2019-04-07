869495a7943402270e7a2753212146c89cdfcba3
spgdaman <gatheru.simon432@gmail.com>
2019-04-07 12:04:51 +0300

    Initial files added, users class defined

---
+++ user_cred_classes.py
@@ -0,0 +1,19 @@
+import string
+import random
+
+class Users:
+
+	'''
+		Class that contains the users methods and attributes
+	'''
+	
+	user_info = []
+	
+	def __init__(self,first,last,password):
+		"""
+			Information needed to create a password saving object
+		"""
+		self.name = name
+		self.platform = platform
+		self.username = username
+		self.password = password
